9d033fe950cbe8852841c34e10a3b38e298320e5
binghamtm <binghamtm1@gmail.com>
2025-03-19 12:57:56 -0600

    Learning activity code

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -7,13 +7,13 @@ class Program
     static void Main(string[] args)
     {
         int userPref = 0;
-        
+
         Console.WriteLine("Welcome to your Journal!");
-        
+
         DateTime theCurrentTime = DateTime.Now;
-        
+
         Journal journal = new Journal();
-        
+
 
         do
         {
@@ -28,40 +28,40 @@ class Program
             string userPrefstring = Console.ReadLine();
             userPref = int.Parse(userPrefstring);
 
-        if (userPref == 1)
-        {
-            Entry entry = new Entry();
-            entry._date = theCurrentTime.ToShortDateString();
-            PromptGenerator newPrompt = new PromptGenerator();
-            entry._promptText = newPrompt.GetRandomPrompt();
-            Console.WriteLine(entry._promptText);
-            entry._entryText = Console.ReadLine();
-            Console.WriteLine("What is one thing you were grateful for today? ");
-            entry._gratefulForText = Console.ReadLine();
-            Console.Write("How happy were you on a scale of 1-10? ");
-            entry._happyLevel = Console.ReadLine();
-            journal.AddEntry(entry);
-        }
-        else if (userPref == 2)
-        {
-            journal.DisplayAll();
-        }
-        else if (userPref == 3)
-        {
-            Console.Write("Enter the file name you want to load in: ");
-            string fileName = Console.ReadLine();
-            journal.LoadFromFile(fileName);
-        }
-        else if (userPref == 4)
-        {
-            Console.Write("Enter a new filename to save: ");
-            string fileName = Console.ReadLine();
-            journal.SaveToFile(fileName);
-        }
+            if (userPref == 1)
+            {
+                Entry entry = new Entry();
+                entry._date = theCurrentTime.ToShortDateString();
+                PromptGenerator newPrompt = new PromptGenerator();
+                entry._promptText = newPrompt.GetRandomPrompt();
+                Console.WriteLine(entry._promptText);
+                entry._entryText = Console.ReadLine();
+                Console.WriteLine("What is one thing you were grateful for today? ");
+                entry._gratefulForText = Console.ReadLine();
+                Console.Write("How happy were you on a scale of 1-10? ");
+                entry._happyLevel = Console.ReadLine();
+                journal.AddEntry(entry);
+            }
+            else if (userPref == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (userPref == 3)
+            {
+                Console.Write("Enter the file name you want to load in: ");
+                string fileName = Console.ReadLine();
+                journal.LoadFromFile(fileName);
+            }
+            else if (userPref == 4)
+            {
+                Console.Write("Enter a new filename to save: ");
+                string fileName = Console.ReadLine();
+                journal.SaveToFile(fileName);
+            }
         } while (userPref != 5);
 
-        Console.WriteLine("Have a great day!") ;  
-    
+        Console.WriteLine("Have a great day!");
+
     }
 
 }
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction = new Fraction();
+        Console.WriteLine(fraction.GetFractionString());
+        fraction = new Fraction(7);
+        Console.WriteLine(fraction.GetFractionString());
+        fraction = new Fraction(7, 11);
+        Console.WriteLine(fraction.GetFractionString());
+
     }
 }
