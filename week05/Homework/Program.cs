3f54bbeca1a2033ff5de3f2f4d4a65e2244241b5
binghamtm <binghamtm1@gmail.com>
2025-04-02 20:30:27 -0600

    Week 5 learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment = new MathAssignment("Section 3939", "problems 56 and 45", "Jeff Bezos", "fractions");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("The causes of World War II", "Joe Mama", "History");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
