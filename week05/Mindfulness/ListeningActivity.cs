4339e07903e843b23e0dfafe1b7339515e4103f4
binghamtm <binghamtm1@gmail.com>
2025-04-05 23:44:11 -0600

    week 5 team activity

---
+++ week05/Mindfulness/ListeningActivity.cs
@@ -0,0 +1,25 @@
+public class ListeningActivity
+{
+    private int _count;
+    private List<string> _prompts = new List<string>();
+
+    public ListeningActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public string GetListFromUser()
+    {
+        return "return list of strings";
+    }
+}
---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,35 @@
+public class ReflectingActivity
+{
+    private List<string> _prompts = new List<string>();
+    private List<string> _questions = new List<string>();
+
+    public ReflectingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "prompt";
+    }
+
+    public string GetRandomQuestion()
+    {
+        return "question";
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+
+    public void DisplayQuestions()
+    {
+        
+    }
+}
