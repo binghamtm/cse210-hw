41071b81bf8c8b8d24943017f3dbac5a2ffb4263
binghamtm <binghamtm1@gmail.com>
2025-04-10 16:14:04 -0600

    team activity

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,27 @@
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, string points) : base(name, description, points)
+    {
+
+    }
+    public override void RecordEvent()
+    {
+        
+    }
+    public override bool IsComplete()
+    {
+        return true;
+    }
+    public override string GetDetailsString()
+    {
+        return "";
+    }
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+}
